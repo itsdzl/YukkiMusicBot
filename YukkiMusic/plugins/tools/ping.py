78afb4950d17f8f8472e6ad793bec5dbf6b86d41
DzLyz <86110711+itsdzl@users.noreply.github.com>
2022-05-02 09:11:03 +0700

    Update ping.py

--- YukkiMusic/plugins/tools/ping.py
+++ YukkiMusic/plugins/tools/ping.py
@@ -30,12 +30,17 @@ PING_COMMAND = get_command("PING_COMMAND")
     & ~BANNED_USERS
 )
 @language
-async def ping(_, message):
-    uptime = await bot_sys_stats()
+async def ping_com(client, message: Message, _):
+    response = await message.reply_photo(
+        photo=PING_IMG_URL,
+        caption=_["ping_1"],
+    )
     start = datetime.now()
-    response = await message.reply_text("ping...")
-    end = datetime.now()
-    resp = (end - start).microseconds / 1000
-    await response.edit(
-        f"**🏓Pong!**\n`⚡{resp} ms`"
+    pytgping = await Yukki.ping()
+    UP, CPU, RAM, DISK = await bot_sys_stats()
+    resp = (datetime.now() - start).microseconds / 1000
+    await response.edit_text(
+        _["ping_2"].format(
+            MUSIC_BOT_NAME, resp, UP, DISK, CPU, RAM, pytgping
+        )
     )
