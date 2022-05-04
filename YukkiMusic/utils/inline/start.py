019a8396b5459f07cc117a5ae5192b76c686c211
DzLyz <86110711+itsdzl@users.noreply.github.com>
2022-05-04 16:47:11 +0700

    Update start.py

--- YukkiMusic/utils/inline/start.py
+++ YukkiMusic/utils/inline/start.py
@@ -43,10 +43,9 @@ def private_panel(_, BOT_USERNAME, OWNER: Union[bool, int] = None):
             InlineKeyboardButton(
                 text=_["S_B_5"],
                 url=f"https://t.me/{BOT_USERNAME}?startgroup=true",
-            ),
-        ],
+            )
+        ]
     )
-    else:
         if SUPPORT_CHANNEL and SUPPORT_GROUP:
         buttons.append(
             [
