17758fb92f900c921ffc07cb6f5afa0f762ae7a1
DzLyz <86110711+itsdzl@users.noreply.github.com>
2022-05-20 12:05:44 +0700

    Update languages.py

--- YukkiMusic/plugins/tools/languages.py
+++ YukkiMusic/plugins/tools/languages.py
@@ -25,7 +25,7 @@ def lanuages_keyboard(_):
     keyboard = InlineKeyboard(row_width=2)
     keyboard.row(
         InlineKeyboardButton(
-            text="🏴󠁧󠁢󠁥󠁮󠁧󠁿 English",
+            text="🇺🇸 English",
             callback_data=f"languages:en",
         ),
         InlineKeyboardButton(
