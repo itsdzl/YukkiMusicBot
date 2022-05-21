9dc3a81e160f3f151b072a5a29a33ff8273e9739
DzLyz <86110711+itsdzl@users.noreply.github.com>
2022-05-22 05:19:03 +0700

    Update play.py

--- YukkiMusic/utils/inline/play.py
+++ YukkiMusic/utils/inline/play.py
@@ -8,7 +8,7 @@
 # All rights reserved.
 
 from pyrogram.types import InlineKeyboardButton
-
+from config import GITHUB_REPO, SUPPORT_CHANNEL, SUPPORT_GROUP
 
 def track_markup(_, videoid, user_id, channel, fplay):
     buttons = [
