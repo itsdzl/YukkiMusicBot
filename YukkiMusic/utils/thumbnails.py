138cd8d100bd36066c83d1a8c506a56069f03d0d
DzLyz <86110711+itsdzl@users.noreply.github.com>
2022-07-16 02:31:47 +0700

    Update thumbnails.py

--- YukkiMusic/utils/thumbnails.py
+++ YukkiMusic/utils/thumbnails.py
@@ -91,7 +91,7 @@ async def gen_thumb(videoid):
         j = 0
         draw.text(
             (600, 150),
-            "DVOTIEN",
+            "NOW PLAYING",
             fill="white",
             stroke_width=2,
             stroke_fill="white",
