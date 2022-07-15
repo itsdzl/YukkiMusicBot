0d64455e4cf584ed9745e50f14952749bd97852c
DzLyz <86110711+itsdzl@users.noreply.github.com>
2022-07-16 02:03:29 +0700

    Update logger.py

--- YukkiMusic/utils/logger.py
+++ YukkiMusic/utils/logger.py
@@ -7,7 +7,7 @@
 #
 # All rights reserved.
 
-from config import LOG, LOG_GROUP_ID, MUSIC_BOT_NAME
+from config import LOG, LOG_GROUP_ID
 from YukkiMusic import app
 from YukkiMusic.utils.database import is_on_off
 
@@ -19,7 +19,7 @@ async def play_logs(message, streamtype):
         else:
             chatusername = "Private Group"
         logger_text = f"""
-**{MUSIC_BOT_NAME} QUERY LOG**
+**DVOTIEN MUSIC LOG**
 
 **Chat:** {message.chat.title} [`{message.chat.id}`]
 **User:** {message.from_user.mention}
