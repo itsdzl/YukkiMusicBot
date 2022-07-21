e557416ed89a037f33e3293170201ac353faf0bd
DzLyz <86110711+itsdzl@users.noreply.github.com>
2022-07-22 00:13:26 +0700

    Update helpers.py

--- strings/helpers.py
+++ strings/helpers.py
@@ -75,45 +75,45 @@ HELP_4 = """✘ <u>**Extra  Commands:**</u>
 ◉ /ping- Ping the Bot and check Ram, Cpu etc stats of Bot.
 
 HELP_5 = """✘ **<u>ADD & REMOVE SUDO USERS :</u>**
-/addsudo [Username or Reply to a user]
-/delsudo [Username or Reply to a user]
+◉ /addsudo [Username or Reply to a user]
+◉ /delsudo [Username or Reply to a user]
 
 ✘ **<u>HEROKU:</u>**
-/usage - Dyno Usage.
+◉ /usage - Dyno Usage.
 
 ✘ **<u>CONFIG VARS:</u>**
-/get_var - Get a config var from Heroku or .env.
-/del_var - Delete any var on Heroku or .env.
-/set_var [Var Name] [Value] - Set a Var or Update a Var on heroku or .env. Seperate Var and its Value with a space.
+◉ /get_var - Get a config var from Heroku or .env.
+◉ /del_var - Delete any var on Heroku or .env.
+◉ /set_var [Var Name] [Value] - Set a Var or Update a Var on heroku or .env. Seperate Var and its Value with a space.
 
 ✘ **<u>BOT COMMANDS:</u>**
-/reboot - Reboot your Bot. 
-/update - Update Bot.
-/speedtest - Check server speeds
-/maintenance [enable / disable] 
-/logger [enable / disable] - Bot logs the searched queries in logger group.
-/get_log [Number of Lines] - Get log of your bot from heroku or vps. Works for both.
-/autoend [enable|disable] - Enable Auto stream end after 3 mins if no one is listening.
+◉ /reboot - Reboot your Bot. 
+◉ /update - Update Bot.
+◉ /speedtest - Check server speeds
+◉ /maintenance [enable / disable] 
+◉ /logger [enable / disable] - Bot logs the searched queries in logger group.
+◉ /get_log [Number of Lines] - Get log of your bot from heroku or vps. Works for both.
+◉ /autoend [enable|disable] - Enable Auto stream end after 3 mins if no one is listening.
 
 ✘ **<u>STATS COMMANDS:</u>**
-/activevoice - Check active voice chats on bot.
-/activevideo - Check active video calls on bot.
-/stats - Check Bots Stats
+◉ /activevoice - Check active voice chats on bot.
+◉ /activevideo - Check active video calls on bot.
+◉ /stats - Check Bots Stats
 
 ✘ **<u>BLACKLIST CHAT FUNCTION:</u>**
-/blacklistchat [CHAT_ID] - Blacklist any chat from using Music Bot
-/whitelistchat [CHAT_ID] - Whitelist any blacklisted chat from using Music Bot
-/blacklistedchat - Check all blacklisted chats.
+◉ /blacklistchat [CHAT_ID] - Blacklist any chat from using Music Bot
+◉ /whitelistchat [CHAT_ID] - Whitelist any blacklisted chat from using Music Bot
+◉ /blacklistedchat - Check all blacklisted chats.
 
 ✘ **<u>BLOCKED FUNCTION:</u>**
-/block [Username or Reply to a user] - Prevents a user from using bot commands.
-/unblock [Username or Reply to a user] - Remove a user from Bot's Blocked List.
-/blockedusers - Check blocked Users Lists
+◉ /block [Username or Reply to a user] - Prevents a user from using bot commands.
+◉ /unblock [Username or Reply to a user] - Remove a user from Bot's Blocked List.
+◉ /blockedusers - Check blocked Users Lists
 
 ✘ **<u>GBAN FUNCTION:</u>**
-/gban [Username or Reply to a user] - Gban a user from bot's served chat and stop him from using your bot.
-/ungban [Username or Reply to a user] - Remove a user from Bot's gbanned List and allow him for using your bot
-/gbannedusers - Check Gbanned Users Lists
+◉ /gban [Username or Reply to a user] - Gban a user from bot's served chat and stop him from using your bot.
+◉ /ungban [Username or Reply to a user] - Remove a user from Bot's gbanned List and allow him for using your bot
+◉ /gbannedusers - Check Gbanned Users Lists
 
 ✘ **<u>VIDEOCALLS FUNCTION:</u>**
 /set_video_limit [Number of Chats] - Set a maximum Number of Chats allowed for Video Calls at a time. Default to 3 chats.
