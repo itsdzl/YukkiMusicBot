1e071ff9594510786824d55b901c8cb30495eae0
DzLyz <86110711+itsdzl@users.noreply.github.com>
2022-07-21 23:13:40 +0700

    Update helpers.py

--- strings/helpers.py
+++ strings/helpers.py
@@ -9,106 +9,70 @@
 
 HELP_1 = """✘ **<u>Admin Commands:</u>**
 
-**c** for channel play.
+**C** stands for channel play.
 
-/pause or /cpause - Pause the playing music.
-/resume or /cresume- Resume the paused music.
-/mute or /cmute- Mute the playing music.
-/unmute or /cunmute- Unmute the muted music.
-/skip or /cskip- Skip the current playing music.
-/stop or /cstop- Stop the playing music.
-/shuffle or /cshuffle- Randomly shuffles the queued playlist.
-/seek or /cseek - Forward Seek the music to your duration
-/seekback or /cseekback - Backward Seek the music to your duration
-/restart - Restart bot for your chat .
-
-
-✘ <u>**Specific Skip:**</u>
-/skip or /cskip [Number(example: 3)] 
-    - Skips music to a the specified queued number. Example: /skip 3 will skip music to third queued music and will ignore 1 and 2 music in queue.
+◉ /pause or /cpause - Pause the playing music.
+◉ /resume or /cresume- Resume the paused music.
+◉ /mute or /cmute- Mute the playing music.
+◉ /unmute or /cunmute- Unmute the muted music.
+◉ /skip or /cskip- Skip the current playing music.
+◉ /stop or /cstop- Stop the playing music.
+◉ /shuffle or /cshuffle- Randomly shuffles the queued playlist.
+◉ /seek or /cseek - Forward Seek the music to your duration
+◉ /seekback or /cseekback - Backward Seek the music to your duration
+◉ /restart - Restart bot for your chat .
 
 ✘ <u>**Loop Play:**</u>
-/loop or /cloop [enable/disable] or [Numbers between 1-10] 
-    - When activated, bot loops the current playing music to 1-10 times on voice chat. Default to 10 times.
+
+◉ /loop or /cloop [1-10] 
+    - When activated, the bot will repeat playback for 1-10 times in voice chat.
 
 ✘ <u>**Auth Users:**</u>
-Auth Users can use admin commands without admin rights in your chat.
 
-/auth [Username] - Add a user to AUTH LIST of the group.
-/unauth [Username] - Remove a user from AUTH LIST of the group.
-/authusers - Check AUTH LIST of the group."""
+Auth users can use admin commands without admin rights in your chats.
+
+◉ /auth [Username] - Add a user to AUTH LIST of the group.
+◉ /unauth [Username] - Remove a user from AUTH LIST of the group.
+◉ /authusers - Check AUTH LIST of the group."""
 
 
 HELP_2 = """✘ <u>**Play Commands:**</u>
 
-Available Commands = play , vplay , cplay
+• Available Commands = play , vplay , cplay
+• ForcePlay Commands = playforce , vplayforce , cplayforce
 
-ForcePlay Commands = playforce , vplayforce , cplayforce
+**C** stands for channel play.
+**V** stands for video play.
+**Force** stands for force play.
 
-**c** stands for channel play.
-**v** stands for video play.
-**force** stands for force play.
-
-/play or /vplay or /cplay  - Bot will start playing your given query on voice chat or Stream live links on voice chats.
-
-/playforce or /vplayforce or /cplayforce -  **Force Play** stops the current playing track on voice chat and starts playing the searched track instantly without disturbing/clearing queue.
-
-/channelplay [Chat username or id] or [Disable] - Connect channel to a group and stream music on channel's voice chat from your group.
+◉ /play or /vplay or /cplay  - Bot will start playing your given query on voice chat or Stream live links on voice chats.
+◉ /playforce or /vplayforce or /cplayforce -  **Force Play** stops the current playing track on voice chat and starts playing the searched track instantly without disturbing/clearing queue.
+◉ /channelplay [Chat username or id] or [Disable] - Connect channel to a group and stream music on channel's voice chat from your group.
 
 
 ✘ **<u>Bot's Server Playlists:</u>**
-/playlist  - Check Your Saved Playlist On Servers.
-/deleteplaylist - Delete any saved music in your playlist
-/play  - Start playing Your Saved Playlist from Servers."""
+◉ /playlist  - Check Your Saved Playlist On Servers.
+◉ /deleteplaylist - Delete any saved music in your playlist
+◉ /play  - Start playing Your Saved Playlist from Servers."""
 
 
 HELP_3 = """✘ <u>**Bot Commands:**</u>
 
-/stats - Get Top 10 Tracks Global Stats, Top 10 Users of bot, Top 10 Chats on bot, Top 10 Played in a chat etc etc.
+◉ /stats - Get Top 10 Tracks Global Stats, Top 10 Users of bot, Top 10 Chats on bot, Top 10 Played in a chat etc etc.
+◉ /sudolist - Check Sudo Users of Bot
+◉ /lyrics [Music Name] - Searches Lyrics for the particular Music on web.
+◉ /song [Track Name] or [YT Link] - Download any track from youtube in mp3 or mp4 formats.
+◉ /player -  Get a interactive Playing Panel.
 
-/sudolist - Check Sudo Users of Yukki Music Bot
+**C** stands for channel play.
 
-/lyrics [Music Name] - Searches Lyrics for the particular Music on web.
-
-/song [Track Name] or [YT Link] - Download any track from youtube in mp3 or mp4 formats.
-
-/player -  Get a interactive Playing Panel.
-
-**c** stands for channel play.
-
-/queue or /cqueue- Check Queue List of Music."""
+◉ /queue or /cqueue- Check Queue List of Streaming."""
 
 HELP_4 = """✘ <u>**Extra  Commands:**</u>
-/start - Start the Music Bot.
-/help  - Get Commands Helper Menu with detailed explanations of commands.
-/ping- Ping the Bot and check Ram, Cpu etc stats of Bot.
 
-✘ <u>**Group Settings:**</u>
-/settings - Get a complete group's settings with inline buttons
-
-🔗 **Options in Settings:**
-
-1️⃣ You can set **Audio Quality** you want to stream on voice chat.
-
-2️⃣ You can set **Video Quality** you want to stream on voice chat.
-
-3️⃣ **Auth Users**:- You can change admin commands mode from here to everyone or admins only. If everyone, anyone present in you group will be able to use admin commands(like /skip, /stop etc)
-
-4️⃣ **Clean Mode:** When enabled deletes the bot's messages after 5 mins from your group to make sure your chat remains clean and good.
-
-5️⃣ **Command Clean** : When activated, Bot will delete its executed commands (/play, /pause, /shuffle, /stop etc) immediately.
-
-6️⃣ **Play Settings:**
-
-/playmode - Get a complete play settings panel with buttons where you can set your group's play settings. 
-
-<u>Options in playmode:</u>
-
-1️⃣ **Search Mode** [Direct or Inline] - Changes your search mode while you give /play mode. 
-
-2️⃣ **Admin Commands** [Everyone or Admins] - If everyone, anyone present in you group will be able to use admin commands(like /skip, /stop etc)
-
-3️⃣ **Play Type** [Everyone or Admins] - If admins, only admins present in group can play music on voice chat."""
+◉ /start - Start the Music Bot.
+◉ /help  - Get Commands Helper Menu with detailed explanations of commands.
+◉ /ping- Ping the Bot and check Ram, Cpu etc stats of Bot.
 
 HELP_5 = """✘ **<u>ADD & REMOVE SUDO USERS :</u>**
 /addsudo [Username or Reply to a user]
