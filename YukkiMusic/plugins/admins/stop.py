023e97b3a0ce46378c8c8d6c138373d909020b14
DzLyz <86110711+itsdzl@users.noreply.github.com>
2022-07-21 07:05:45 +0700

    Update stop.py

--- YukkiMusic/plugins/admins/stop.py
+++ YukkiMusic/plugins/admins/stop.py
@@ -34,5 +34,4 @@ async def stop_music(cli, message: Message, _, chat_id):
     await Yukki.stop_stream(chat_id)
     await set_loop(chat_id, 0)
     await message.reply_text(
-        _["admin_9"].format(message.from_user.mention)
-    )
+        _["admin_9"])
