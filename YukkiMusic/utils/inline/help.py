4eebc9ce1cfaae43c5413d2a690519435f193c47
DzLyz <86110711+itsdzl@users.noreply.github.com>
2022-07-24 13:58:37 +0700

    Update help.py

--- YukkiMusic/utils/inline/help.py
+++ YukkiMusic/utils/inline/help.py
@@ -44,12 +44,14 @@ def help_pannel(_, START: Union[bool, int] = None):
             ],
             [
                 InlineKeyboardButton(
-                    text=_["H_B_6"],
-                    callback_data="help_callback hb5",
-                ),
-                InlineKeyboardButton(
                     text=_["H_B_4"],
                     callback_data="help_callback hb4",
+                ),
+            ],
+            [
+                InlineKeyboardButton(
+                    text=_["H_B_6"],
+                    callback_data="help_callback hb5",
                 ),
             ],
             mark,
