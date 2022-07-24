ba54a48a3a2f7d27f9eb5957f2e354e66707338c
DzLyz <86110711+itsdzl@users.noreply.github.com>
2022-07-24 13:06:31 +0700

    Update help.py

--- YukkiMusic/utils/inline/help.py
+++ YukkiMusic/utils/inline/help.py
@@ -66,9 +66,6 @@ def help_back_markup(_):
                     text=_["BACK_BUTTON"],
                     callback_data=f"settings_back_helper",
                 ),
-                InlineKeyboardButton(
-                    text=_["CLOSE_BUTTON"], callback_data=f"close"
-                ),
             ]
         ]
     )
