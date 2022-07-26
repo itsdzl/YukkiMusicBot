18081765a4308cc80642224d47f21eaec0fd6e5e
DzLyz <86110711+itsdzl@users.noreply.github.com>
2022-07-27 01:51:21 +0700

    Update start.py

--- YukkiMusic/plugins/bot/start.py
+++ YukkiMusic/plugins/bot/start.py
@@ -195,6 +195,7 @@ async def start_comm(client, message: Message, _):
         out = private_panel(_, app.username, OWNER)
         if config.START_IMG_URL:
             try:
+                await message.reply_sticker("CAACAgUAAx0CQZS2QQABG-0ZYuA3VPTMXIOQ2OFKkfw_abg0LYEAAvICAAJVn9BUD7CVUBAFvkspBA")
                 await message.reply_photo(
                     photo=config.START_IMG_URL,
                     caption=_["start_2"].format(
