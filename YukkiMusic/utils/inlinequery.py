7499b337d76866a7d11901209233a011d5c9c6a1
DzLyz <86110711+itsdzl@users.noreply.github.com>
2022-07-27 03:09:05 +0700

    Update inlinequery.py

--- YukkiMusic/utils/inlinequery.py
+++ YukkiMusic/utils/inlinequery.py
@@ -17,7 +17,7 @@ answer.extend(
         InlineQueryResultArticle(
             title="Youtube Search",
             description=f"Give YouTube music or video name.",
-            thumb_url="https://telegra.ph/file/c0a1c789def7b93f13745.png",
+            thumb_url="https://telegra.ph/file/a3866f5534588b97a9fc3.png",
             input_message_content=InputTextMessageContent("/help@dzmusicbot"),
         ),
     ]
